229d21b0c6af42713fcf597f0d7aa81f031d94c4
Akhilesh26 <akhileshpimple3@gmail.com>
2025-07-30 11:23:40 +0530

    update controller with initial logic of log in

--- services/user-management-service/src/main/java/com/example/user_management_service/Controller/UserLogInController.java
+++ services/user-management-service/src/main/java/com/example/user_management_service/Controller/UserLogInController.java
@@ -1,4 +1,36 @@
 package com.example.user_management_service.Controller;
 
+
+import com.example.user_management_service.DTO.UserLogInDTO;
+import com.example.user_management_service.Service.UserLogInService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/api/user-management-service/log-in-service")
 public class UserLogInController {
+
+    public UserLogInService userLogInService;
+    public UserLogInController(UserLogInService userLogInService){
+        this.userLogInService = userLogInService;
+    }
+
+
+    @GetMapping("/greet")
+    public ResponseEntity<String> greet(){
+        return ResponseEntity.ok("Hey, from user management service");
+    }
+
+    @PostMapping("/username-login")
+    public ResponseEntity<String> handleUsernameLogIn(@RequestBody UserLogInDTO userLogInDTO){
+        if(userLogInDTO.getUsername() == null || userLogInDTO.getPassword() == null){
+            return ResponseEntity.badRequest().body("Wrong Credentials");
+        }
+
+        if(userLogInService.userLogInCheck(userLogInDTO))
+            return ResponseEntity.accepted().body("Success");
+
+        return ResponseEntity.internalServerError().body("Server Crashed");
+    }
 }
