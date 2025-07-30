2ed0e9af8e16d638f5423321d8477bea4874aa1a
Akhilesh26 <akhileshpimple3@gmail.com>
2025-07-30 11:22:25 +0530

    add Repo

---
+++ services/user-management-service/src/main/java/com/example/user_management_service/Repo/UserLoginRepository.java
@@ -0,0 +1,19 @@
+package com.example.user_management_service.Repo;
+
+
+import com.example.user_management_service.DTO.UserLogInDTO;
+import com.example.user_management_service.Model.UserModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.UUID;
+
+@Repository
+public interface UserLoginRepository extends JpaRepository<UserModel,UUID> {
+
+    boolean existsUsername(String username);
+    boolean existsPasswordHash(String password);
+
+
+}
