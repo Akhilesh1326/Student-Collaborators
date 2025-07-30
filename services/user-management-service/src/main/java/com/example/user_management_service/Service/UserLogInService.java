bc9fa241fe7dbea52698f3acf534754d75e1ce13
Akhilesh26 <akhileshpimple3@gmail.com>
2025-07-30 12:51:55 +0530

    update for add password encoder

--- services/user-management-service/src/main/java/com/example/user_management_service/Service/UserLogInService.java
+++ services/user-management-service/src/main/java/com/example/user_management_service/Service/UserLogInService.java
@@ -4,11 +4,15 @@ package com.example.user_management_service.Service;
 import com.example.user_management_service.DTO.UserLogInDTO;
 import com.example.user_management_service.Enums.RegistrationStatusEnum;
 import com.example.user_management_service.Repo.UserLoginRepository;
-import com.example.user_management_service.Repo.UserRegisterRepository;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
+
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserLogInService {
+
+    private PasswordEncoder passwordEncoder;
 
     private UserLoginRepository userLoginRepository;
     public UserLogInService(UserLoginRepository userLoginRepository){
@@ -18,7 +22,8 @@ public class UserLogInService {
         if(userLoginRepository.existsByUsername(userLogInDTO.getUsername())) {
             return RegistrationStatusEnum.RegisterStatus.USERNAME_EXISTS;
         }
-        if(userLoginRepository.existsByPassword(userLogInDTO.getPassword())){
+        String encodedPass = passwordEncoder.encode(userLogInDTO.getPassword());
+        if(userLoginRepository.existsByPassword(encodedPass)){
             return RegistrationStatusEnum.RegisterStatus.SUCCESS;
         }
 
