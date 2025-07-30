f3d585b0deda2c7a05a1d15776ed6542797295bd
Akhilesh26 <akhileshpimple3@gmail.com>
2025-07-30 12:44:43 +0530

    package for enums

---
+++ services/user-management-service/src/main/java/com/example/user_management_service/Enums/RegistrationStatusEnum.java
@@ -0,0 +1,10 @@
+package com.example.user_management_service.Enums;
+
+public class RegistrationStatusEnum {
+    public enum RegisterStatus {
+        SUCCESS,
+        EMAIL_EXISTS,
+        USERNAME_EXISTS,
+        ERROR
+    }
+}
